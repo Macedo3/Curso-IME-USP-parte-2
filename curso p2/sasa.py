824c4a9487c7ff18aad4f4e04fbf7a274a1ef0ad
Gabriel de Macedo Santos <82056067+Macedo3@users.noreply.github.com>
2021-09-20 13:43:40 -0300

    Delete curso p2 directory

--- curso p2/sasa.py
+++
@@ -1,4 +0,0 @@
-def Start(msg):
-    print(msg) 
-
-Start('Hello, world!')
--- curso p2/semana 2/maiuscula.py
+++
@@ -1,8 +0,0 @@
-import string
-
-def maiusculas(a_string):
-    uppers = ""
-    for c in a_string:
-        if c in string.ascii_uppercase:
-            uppers = uppers + c
-    return uppers
